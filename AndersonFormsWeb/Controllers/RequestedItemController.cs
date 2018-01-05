62e5466d27565f52b93af01edb0b72ba4fd6db8b
Robert Sanchez <robertbriansanchez@outlook.ph>
2018-01-05 16:27:59 +0800

    BYOD (fix)

--- AndersonFormsWeb/Controllers/RequestedItemController.cs
+++ AndersonFormsWeb/Controllers/RequestedItemController.cs
@@ -1,6 +1,4 @@
-
-﻿
-using System;
+﻿using System;
 using System.Web.Mvc;
 using System.Linq;
 using AndersonFormsFunction;
@@ -33,7 +31,7 @@ namespace AndersonFormsWeb.Controllers
             return View();
         }
 
-        
+
 
         [HttpGet]
         public ActionResult Create()
@@ -43,7 +41,7 @@ namespace AndersonFormsWeb.Controllers
 
         [HttpPost]
 
-        public JsonResult Create(int id ,RollInModel rollInModel)
+        public JsonResult Create(int id, RollInModel rollInModel)
 
         {
 
@@ -66,7 +64,7 @@ namespace AndersonFormsWeb.Controllers
             }
 
         }
-       
+
 
         [Route("List")]
         [HttpPost]
@@ -85,90 +83,4 @@ namespace AndersonFormsWeb.Controllers
 
 
     }
-
-﻿
-using System;
-using System.Web.Mvc;
-using System.Linq;
-using AndersonFormsFunction;
-using AndersonFormsModel;
-using AndersonFormsWeb.Models;
-using AndersonCRMFunction;
-
-namespace AndersonFormsWeb.Controllers
-{
-
-    [RoutePrefix("RequestedItem")]
-    public class RequestedItemController : Controller
-    {
-        private IFRequestedItem _iFRequestedItem;
-        private IFEmployee _iFEmployee;
-
-        public RequestedItemController()
-        {
-            _iFRequestedItem = new FRequestedItem();
-            _iFEmployee = new FEmployee();
-        }
-
-        [Route("")]
-        [HttpGet]
-        public ActionResult Index()
-        {
-
-            return View();
-        }
-
-        
-
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return View(new RequestedItem());
-        }
-
-        [HttpPost]
-
-        public JsonResult Create(int id ,RollInModel rollInModel)
-
-        {
-
-            try
-            {
-
-
-                var employee = _iFEmployee.Create(id, rollInModel.Employee);
-
-                foreach (var requestedItem in rollInModel.RequestedItems)
-                {
-                    requestedItem.EmployeeId = employee.EmployeeId;
-                    _iFRequestedItem.Create(requestedItem);
-                }
-                return Json("");
-            }
-            catch (Exception ex)
-            {
-                return Json(ex);
-            }
-
-        }
-       
-
-        [Route("List")]
-        [HttpPost]
-        public JsonResult List()
-        {
-            try
-            {
-                RequestedItem requestedItem = new RequestedItem();
-                return Json(_iFRequestedItem.List());
-            }
-            catch (Exception exception)
-            {
-                return Json(exception);
-            }
-        }
-
-
-    }
-
 }
